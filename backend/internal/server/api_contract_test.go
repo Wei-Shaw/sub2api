4bba66c08c6e899b825b6df8835a8a71ba28685f
LLLLLLiulei <1065070665@qq.com>
2026-01-14 19:56:19 +0800

    test: fix proxy repo stub

--- backend/internal/server/api_contract_test.go
+++ backend/internal/server/api_contract_test.go
@@ -858,6 +858,10 @@ func (stubProxyRepo) CountAccountsByProxyID(ctx context.Context, proxyID int64) 
 	return 0, errors.New("not implemented")
 REDACTED
 
+func (stubProxyRepo) ListAccountSummariesByProxyID(ctx context.Context, proxyID int64) ([]service.ProxyAccountSummary, error) {
+	return nil, errors.New("not implemented")
+REDACTED
+
 type stubRedeemCodeRepo struct{REDACTED
 
 func (stubRedeemCodeRepo) Create(ctx context.Context, code *service.RedeemCode) error {
