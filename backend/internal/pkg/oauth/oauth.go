275756f5d17c37f3afc8039b7ce3427c36917dce
shaw <shaw-wei@foxmail.com>
2026-01-23 15:17:47 +0800

    feat: 保存并显示OAuth账号邮箱地址

--- backend/internal/pkg/oauth/oauth.go
+++ backend/internal/pkg/oauth/oauth.go
@@ -215,5 +215,6 @@ REDACTED
 
 // AccountInfo represents account info from OAuth response
 type AccountInfo struct {
-	UUID string `json:"uuid"`
+	UUID         string `json:"uuid"`
+	EmailAddress string `json:"email_address"`
 REDACTED
