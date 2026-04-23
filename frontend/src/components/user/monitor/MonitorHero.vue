d896e0b1d66277107fcee68ef9755b47deaf6f9b
erio <asakifeng@gmail.com>
2026-04-24 00:08:57 +0800

    fix(monitor): remove redundant "updated at" label from MonitorHero

--- frontend/src/components/user/monitor/MonitorHero.vue
+++ frontend/src/components/user/monitor/MonitorHero.vue
@@ -51,10 +51,6 @@
         @update:enabled="autoRefresh.setEnabled"
         @update:interval="autoRefresh.setInterval"
       />
-
-      <div class="text-xs text-gray-500 dark:text-gray-400 tabular-nums">
-        {{ updatedLabel REDACTEDREDACTED
-      </div>
     </div>
   </section>
 </template>
