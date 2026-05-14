106d227744d5ecbe97efbf1e1642171ce587ce19
Y0 <y0962464@gmail.com>
2026-05-14 22:44:56 +0800

    refactor: 移除 AppSidebar 组件中的版本徽章和相关样式测试

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -15,8 +15,6 @@
         <router-link to="/home" class="sidebar-brand-title block text-lg font-bold text-gray-900 dark:text-white no-underline">
           {{ siteName }}
         </router-link>
-        <!-- Version Badge -->
-        <VersionBadge :version="siteVersion" />
       </div>
     </div>
 
@@ -183,7 +181,6 @@ import { computed, h, onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useAdminSettingsStore, useAppStore, useAuthStore, useOnboardingStore } from '@/stores'
-import VersionBadge from '@/components/common/VersionBadge.vue'
 import { sanitizeSvg } from '@/utils/sanitize'
 import { FeatureFlags, makeSidebarFlag } from '@/utils/featureFlags'
 
@@ -243,7 +240,6 @@ const expandedGroups = ref<Set<string>>(new Set())
 // Site settings from appStore (cached, no flicker)
 const siteName = computed(() => appStore.siteName)
 const siteLogo = computed(() => appStore.siteLogo)
-const siteVersion = computed(() => appStore.siteVersion)
 const settingsLoaded = computed(() => appStore.publicSettingsLoaded)
 
 // SVG Icon Components
