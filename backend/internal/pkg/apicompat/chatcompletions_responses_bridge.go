5f6ab9750366d5470dcd9949cfd2b65c03837761
CHOS1N <zhuzenan02@gmail.com>
2026-07-05 13:58:08 +0800

    Add response_format compatibility mapping

--- backend/internal/pkg/apicompat/chatcompletions_responses_bridge.go
+++ backend/internal/pkg/apicompat/chatcompletions_responses_bridge.go
@@ -38,6 +38,9 @@ REDACTED
 	if len(req.ToolChoice) > 0 {
 		out.ToolChoice = responsesToolChoiceToChatToolChoice(req.ToolChoice)
 REDACTED
+	if req.Text != nil {
+		out.ResponseFormat = responsesTextFormatToChatResponseFormat(req.Text.Format)
+REDACTED
 
 	return out, nil
 REDACTED
