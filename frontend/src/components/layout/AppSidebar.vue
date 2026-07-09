a5c2ea08cfd4d5f590d77b42340db27b41f2f8fc
li <fengshao1227@gmail.com>
2026-07-09 17:09:04 +0800

    fix(frontend): 补齐 en 语言包缺失 key 并对 site_logo 统一应用 sanitizeUrl

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -194,6 +194,7 @@ import { useI18n REDACTED from 'vue-i18n'
 import { useAdminSettingsStore, useAppStore, useAuthStore, useOnboardingStore REDACTED from '@/stores'
 import VersionBadge from '@/components/common/VersionBadge.vue'
 import { sanitizeSvg REDACTED from '@/utils/sanitize'
+import { sanitizeUrl REDACTED from '@/utils/url'
 import { FeatureFlags, makeSidebarFlag REDACTED from '@/utils/featureFlags'
 import { useBatchImageAccess REDACTED from '@/composables/useBatchImageAccess'
 
@@ -256,7 +257,7 @@ const expandedGroups = ref<Set<string>>(new Set())
 
 // Site settings from appStore (cached, no flicker)
 const siteName = computed(() => appStore.siteName)
-const siteLogo = computed(() => appStore.siteLogo)
+const siteLogo = computed(() => sanitizeUrl(appStore.siteLogo || '', { allowRelative: true, allowDataUrl: true REDACTED))
 const siteVersion = computed(() => appStore.siteVersion)
 const settingsLoaded = computed(() => appStore.publicSettingsLoaded)
 
