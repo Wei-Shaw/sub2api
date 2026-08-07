5b56ea786e50be1770d33eba9ac716ddf93ea16f
Pana <pana.wang@outlook.com>
2026-08-07 18:37:23 +0800

    feat(web3deposit): derive evm addresses from account xpub

---
+++ backend/internal/web3deposit/address_deriver.go
@@ -0,0 +1,63 @@
+package web3deposit
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/btcsuite/btcd/btcutil/hdkeychain"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	ErrAccountXPubInvalid        = errors.New("web3 deposit account xpub is invalid")
+	ErrDerivationIndexOutOfRange = errors.New("web3 deposit derivation index is out of range")
+	ErrAddressDerivationFailed   = errors.New("web3 deposit address derivation failed")
+)
+
+type DerivedEVMAddress struct {
+	DerivationIndex   int64
+	Address           string
+	NormalizedAddress string
+}
+
+func DeriveEVMAddress(accountXPub string, derivationIndex int64) (DerivedEVMAddress, error) {
+	if derivationIndex < 0 || derivationIndex >= MaxDerivationIndexExclusive {
+		return DerivedEVMAddress{}, ErrDerivationIndexOutOfRange
+	}
+
+	accountKey, err := hdkeychain.NewKeyFromString(strings.TrimSpace(accountXPub))
+	if err != nil {
+		return DerivedEVMAddress{}, ErrAccountXPubInvalid
+	}
+	defer accountKey.Zero()
+	if accountKey.IsPrivate() || accountKey.Depth() != 3 || accountKey.ChildIndex() < hdkeychain.HardenedKeyStart {
+		return DerivedEVMAddress{}, ErrAccountXPubInvalid
+	}
+
+	externalKey, err := accountKey.Derive(0)
+	if err != nil {
+		return DerivedEVMAddress{}, fmt.Errorf("%w: derive external branch", ErrAddressDerivationFailed)
+	}
+	defer externalKey.Zero()
+	childKey, err := externalKey.Derive(uint32(derivationIndex))
+	if err != nil {
+		return DerivedEVMAddress{}, fmt.Errorf("%w: derive child", ErrAddressDerivationFailed)
+	}
+	defer childKey.Zero()
+
+	publicKey, err := childKey.ECPubKey()
+	if err != nil {
+		return DerivedEVMAddress{}, fmt.Errorf("%w: read child public key", ErrAddressDerivationFailed)
+	}
+	uncompressedPublicKey := publicKey.SerializeUncompressed()
+	hash := crypto.Keccak256(uncompressedPublicKey[1:])
+	address := common.BytesToAddress(hash[12:]).Hex()
+
+	return DerivedEVMAddress{
+		DerivationIndex:   derivationIndex,
+		Address:           address,
+		NormalizedAddress: strings.ToLower(address),
+	}, nil
+}
